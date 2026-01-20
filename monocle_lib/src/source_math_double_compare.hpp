07797b1083e3ac16411d4c8b5409cb7dd3ff3ead
UncraftedName <uncraftedname@gmail.com>
2026-01-19 19:19:40 -0800

    Rename chain dump columns, add portal colors, and increase precision

--- monocle_lib/src/source_math_double_compare.hpp
+++ monocle_lib/src/source_math_double_compare.hpp
@@ -34,31 +34,35 @@ inline std::string TeleportChainCompareToCsv(const TeleportChainParams& params, 
     const PortalPair& pp = *params.pp;
     PortalPairD ppd{pp};
 
-    std::string res = "name,float,double,ulp_diff,abs_diff,rel_diff\n";
+    std::string res = "name,float_val,double_val,ulp_diff,abs_diff,rel_diff,is_blue\n";
     auto it = std::back_inserter(res);
 
-    auto write_row = [&it](const std::string& entry_name, float float_val, double double_val) -> void {
+    auto write_row = [&it](const std::string& entry_name, float float_val, double double_val, bool blue) -> void {
         it = std::format_to(it,
-                            "{}," MON_F_FMT "," MON_D_FMT ",{:.2f}," MON_D_FMT "," MON_D_FMT "\n",
+                            "{}," MON_F_FMT "," MON_D_FMT "," MON_D_FMT "," MON_D_FMT "," MON_D_FMT
+                            ",{}"
+                            "\n",
                             entry_name,
                             float_val,
                             double_val,
                             UlpDiffD(double_val, float_val),
                             double_val - float_val,
-                            (double_val == 0. && float_val == 0.f) ? 0. : (float_val - double_val) / double_val);
+                            (double_val == 0. && float_val == 0.f) ? 0. : (float_val - double_val) / double_val,
+                            blue ? "true" : "false");
     };
 
-    auto write_vec = [&](const std::string& prefix, const Vector& v, const VectorD& vd) {
+    auto write_vec = [&](const std::string& prefix, const Vector& v, const VectorD& vd, bool blue) {
         const char* elem_chars = "xyz";
         for (size_t i = 0; i < 3; i++)
-            write_row(prefix + "_" + elem_chars[i], v[i], vd[i]);
+            write_row(prefix + "_" + elem_chars[i], v[i], vd[i], blue);
     };
 
-    auto write_mat = [&](const std::string& prefix, const float* m, const double* md, size_t rows, size_t cols) {
-        for (size_t r = 0; r < rows; r++)
-            for (size_t c = 0; c < cols; c++)
-                write_row(prefix + "_" + std::to_string(r * cols + c), m[r * cols + c], md[r * cols + c]);
-    };
+    auto write_mat =
+        [&](const std::string& prefix, const float* m, const double* md, size_t rows, size_t cols, bool blue) {
+            for (size_t r = 0; r < rows; r++)
+                for (size_t c = 0; c < cols; c++)
+                    write_row(prefix + "_" + std::to_string(r * cols + c), m[r * cols + c], md[r * cols + c], blue);
+        };
 
     // write portal vals
 
@@ -67,32 +71,35 @@ inline std::string TeleportChainCompareToCsv(const TeleportChainParams& params, 
         const Portal& p = is_blue ? pp.blue : pp.orange;
         const PortalD& pd = is_blue ? ppd.blue : ppd.orange;
 
-        write_vec(prefix + "pos", p.pos, pd.pos);
-        write_vec(prefix + "ang", *(Vector*)&p.ang, *(VectorD*)&pd.ang);
-        write_vec(prefix + "f", p.f, pd.f);
-        write_vec(prefix + "r", p.r, pd.r);
-        write_vec(prefix + "u", p.u, pd.u);
-        write_vec(prefix + "plane_n", p.plane.n, pd.plane.n);
-        write_row(prefix + "plane_d", p.plane.d, pd.plane.d);
-        write_mat(prefix + "mat", &p.mat[0][0], &pd.mat[0][0], 3, 4);
+        write_vec(prefix + "pos", p.pos, pd.pos, is_blue);
+        write_vec(prefix + "ang", *(Vector*)&p.ang, *(VectorD*)&pd.ang, is_blue);
+        write_vec(prefix + "f", p.f, pd.f, is_blue);
+        write_vec(prefix + "r", p.r, pd.r, is_blue);
+        write_vec(prefix + "u", p.u, pd.u, is_blue);
+        write_vec(prefix + "plane_n", p.plane.n, pd.plane.n, is_blue);
+        write_row(prefix + "plane_d", p.plane.d, pd.plane.d, is_blue);
+        write_mat(prefix + "mat", &p.mat[0][0], &pd.mat[0][0], 3, 4, is_blue);
 
         const VMatrix& vm = is_blue ? pp.o_to_b : pp.b_to_o;
         const VMatrixD& vmd = is_blue ? ppd.o_to_b : ppd.b_to_o;
-        write_mat(prefix + "tp_mat", &vm[0][0], &vmd[0][0], 4, 4);
+        write_mat(prefix + "tp_mat", &vm[0][0], &vmd[0][0], 4, 4, is_blue);
     }
 
     // write chain vals
 
     for (size_t i = 0; i < result.tp_dirs.size(); i++) {
+        bool is_blue = params.first_tp_from_blue == result.tp_dirs[i];
         EntityD ent_d_pre{result.ents[i]};
         write_vec(std::string("ent_pre_teleport_") + std::to_string(i),
                   result.ents[i].GetCenter(),
-                  ent_d_pre.GetCenter());
+                  ent_d_pre.GetCenter(),
+                  is_blue);
 
-        EntityD ent_d_post = ppd.Teleport(ent_d_pre, params.first_tp_from_blue == result.tp_dirs[i]);
+        EntityD ent_d_post = ppd.Teleport(ent_d_pre, is_blue);
         write_vec(std::string("ent_post_teleport_") + std::to_string(i),
                   result.ents[i + 1].GetCenter(),
-                  ent_d_post.GetCenter());
+                  ent_d_post.GetCenter(),
+                  is_blue);
     }
 
     return res;
