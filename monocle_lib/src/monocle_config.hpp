b603dd50095a3a974a94e94df7f7a97b1f701f7d
UncraftedName <uncraftedname@gmail.com>
2026-03-03 16:09:20 -0800

    Prepare library for thirdparty cmake usage

--- monocle_lib/src/monocle_config.hpp
+++ monocle_lib/src/monocle_config.hpp
@@ -1,17 +1,31 @@
 #pragma once
 
-#include <utility>
-#include <assert.h>
+#ifdef MON_CFG_INCLUDE
+#include MON_CFG_INCLUDE
+#endif
 
 #define MON_LIB_VERSION_MAJOR 1
 #define MON_LIB_VERSION_MINOR 0
 
+#ifndef MON_F_FMT
 #define MON_F_FMT "{:.9g}"
+#endif
+#ifndef MON_D_FMT
 #define MON_D_FMT "{:.17g}"
+#endif
 
+#if !defined(MON_ASSERT) || !defined(MON_ASSERT_MSG)
+#include <assert.h>
+#ifndef MON_ASSERT
 #define MON_ASSERT(x) assert(x)
+#endif
+#ifndef MON_ASSERT_MSG
 #define MON_ASSERT_MSG(x, msg) assert((msg, (x)))
+#endif
+#endif
 
+#ifndef MON_UNREACHABLE
+#include <utility>
 #ifdef __cpp_lib_unreachable
 #define MON_UNREACHABLE() std::unreachable()
 #else
@@ -20,4 +34,5 @@
         MON_ASSERT(0);    \
         __assume(0);      \
     } while (0)
+#endif
 #endif
